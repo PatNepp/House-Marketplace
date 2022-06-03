a21ef64cd9f75cc4a65892c7f344614fa7d8d75b
Patrick Nepp <ptnepp@gmail.com>
2022-06-03 15:57:52 -0500

    sign in and up and log out functionality

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Offers from './pages/Offers';
 import Profile from './pages/Profile';
 import ForgotPassword from './pages/ForgotPassword';
 import Navbar from './components/Navbar';
-
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
@@ -15,13 +16,15 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
-          <Route path='/profile' element={<SignIn />} />
+          <Route path='/profile' element={<Profile />} />
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
         </Routes>
         <Navbar />
       </Router> 
+
+      <ToastContainer />
     </>
   );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,34 @@
+import {getAuth} from 'firebase/auth'
+import {useState, useEffect} from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+
 function Profile() {
+  const auth = getAuth()
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email
+  })
+
+  const {name, email} = formData
+
+  const navigate = useNavigate()
+
+  const onLogout = () => {
+    auth.signOut()
+    navigate('/')
+  }
+
   return (
-    <div>Profile</div>
+    <div className='profile'>
+      <header className="profileHeader">
+        <p className="pageHeader">
+          My Profile
+        </p>
+        <button type='button' onClick={onLogout} className="logOut">
+          Log Out
+        </button>
+      </header>
+    </div>
   )
 }
 
